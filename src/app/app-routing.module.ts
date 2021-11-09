ec86a1154a355030ccfd5d9d666b5982b7c74223
heiitalo <italovieira.008@gmail.com>
2021-11-09 16:26:49 -0300

    [feature: Italo Task: consuming API cadastrar component]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
+import { InicioComponent } from './inicio/inicio.component';
 import { SobreComponent } from './sobre/sobre.component';
 
 const routes: Routes = [
+{path: '', redirectTo: 'inicio', pathMatch: 'full'},   
+{path: 'inicio', component: InicioComponent },
 {path: 'entrar', component: EntrarComponent },
 {path: 'sobre', component: SobreComponent },
 {path: 'cadastrar', component: CadastrarComponent },
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { CredenciaisDTO } from '../model/CredenciaisDTO';
+import { UsuarioModel } from '../model/UsuarioModel';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +18,16 @@ export class AuthService {
     return this.http.post<CredenciaisDTO>('https://projetocleanenergy.herokuapp.com/usuarios/logar', credenciais)
   }
 
+  cadastrar(usuario: UsuarioModel): Observable<UsuarioModel>{
+    return this.http.post<UsuarioModel>('https://projetocleanenergy.herokuapp.com/usuarios/salvar', usuario)
+  }
 
+  logado(){
+    let ok = false
+  
+    if(environment.token !=''){
+      ok= true
+    }
+    return ok
+  }
 }
