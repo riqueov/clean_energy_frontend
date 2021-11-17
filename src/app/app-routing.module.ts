86310d856b0a6e8a8ab88671785cd9fd59aee9b3
PatriciaTorresGraciano <patricia.dstorres@hotmail.com>
2021-11-17 19:05:04 -0300

    [Feature: Patricia - Task: Building home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,12 @@ import { NovaCategoriaComponent } from './nova-categoria/nova-categoria.componen
 import { NovoProdutoComponent } from './novo-produto/novo-produto.component';
 import { PerfilComponent } from './perfil/perfil.component';
 import { ProdutosComponent } from './produtos/produtos.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ProdutoComponent } from './produto/produto.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { CategoriaComponent } from './categoria/categoria.component';
 
 const routes: Routes = [
 
@@ -14,26 +20,13 @@ const routes: Routes = [
     { path: 'produtos', component: ProdutosComponent},
     { path: 'novo-produto', component: NovoProdutoComponent},
     { path: 'perfil', component: PerfilComponent},
+    {path: 'inicio', component: InicioComponent },
+    {path: 'entrar', component: EntrarComponent },
+    {path: 'sobre', component: SobreComponent },
+    {path: 'cadastrar', component: CadastrarComponent },
+    {path: 'categoria', component: CategoriaComponent},
+    {path: 'produto', component: ProdutoComponent},
  
-
-import { EntrarComponent } from './entrar/entrar.component';
-import { InicioComponent } from './inicio/inicio.component';
-import { ProdutoComponent } from './produto/produto.component';
-import { SobreComponent } from './sobre/sobre.component';
-
-const routes: Routes = [
-{path: '', redirectTo: 'inicio', pathMatch: 'full'},   
-{path: 'inicio', component: InicioComponent },
-{path: 'entrar', component: EntrarComponent },
-{path: 'sobre', component: SobreComponent },
-{path: 'cadastrar', component: CadastrarComponent },
-{path: 'categoria', component: CategoriaComponent},
-{path: 'produto', component: ProdutoComponent},
-
-
-
-
-
 ];
 @NgModule({
  imports: [RouterModule.forRoot(routes)],
