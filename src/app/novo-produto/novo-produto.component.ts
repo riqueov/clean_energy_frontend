a2fa0f221382027c509bb0d89c8dabef9e28bdfc
Ruan <sdias.ruan@gmail.com>
2021-11-18 10:28:56 -0300

    Criado produto service

--- src/app/novo-produto/novo-produto.component.ts
+++ src/app/novo-produto/novo-produto.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProdutoModel } from '../model/ProdutoModel';
 
 @Component({
   selector: 'app-novo-produto',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NovoProdutoComponent implements OnInit {
 
-  constructor() { }
+  produto: ProdutoModel = new ProdutoModel
+
+  constructor(
+    private router: Router,
+    private produtoService: ProdutoService
+  ) { }
 
   ngOnInit(): void {
   }
+
+
 
 }
